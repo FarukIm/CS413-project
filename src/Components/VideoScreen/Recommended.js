3cb873fe7a57f15b1656f796107236a2dc203e8a
FarukIm <f.imamovic27@gmail.com>
2022-05-12 20:48:49 +0200

    small fix regarding sizing of recommended

--- src/Components/VideoScreen/Recommended.js
+++ src/Components/VideoScreen/Recommended.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Recommended = ({ name }) => {
 	return (
-		<div className='w-5/12 md:w-full h-14 border-2 border-black mx-4 my-4'>
+		<div className='w-5/12 md:w-full h-14 border-2 border-black mx-2 my-4'>
 			{name}
 		</div>
 	);
