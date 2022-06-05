bb10006aa5d5ed1b63653012e3708bd84c79ddff
FarukIm <f.imamovic27@gmail.com>
2022-06-05 14:31:42 +0200

    added logout button to header

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,18 +1,35 @@
 import { Link } from "react-router-dom";
-
+import { isLoggedIn } from "../lib/functions";
 const Header = () => {
 	return (
 		<div className='w-full'>
 			<div
-				className='w-full  h-15 p-3 items-center 
-			bg-cyan-600 text-white fixed top-0
+				className='w-full  h-15 p-3
+							bg-cyan-600 text-white fixed top-0 
+							flex flex-row justify-between
 			'
 			>
 				<Link to='/'>
-					<li className='border-2 border-gray-800 px-2 w-fit hover:bg-cyan-300 hover:text-black transition'>
+					<div
+						className='border-2 border-gray-800 px-2 w-fit flex 
+									hover:bg-cyan-300 hover:text-black transition'
+					>
 						Video Player Site
-					</li>
+					</div>
 				</Link>
+
+				<div className={isLoggedIn() ? "block" : "hidden"}>
+					<Link to='/login'>
+						<button
+							onClick={() => {
+								localStorage.removeItem("DUMMY_JWT");
+							}}
+							class='border-2 border-gray-800 px-2 w-fit hover:bg-cyan-300 hover:text-black transition text-right'
+						>
+							Logout
+						</button>
+					</Link>
+				</div>
 			</div>
 		</div>
 	);
